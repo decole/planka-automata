fa373cc33b362fa4de552d30fb4ec1199f51a763
decole <decole2014@yandex.ru>
2023-05-17 01:34:23 +0300

    init project

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: "home")]
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+        ]);
+    }
+}
